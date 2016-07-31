0e0e9ed1ac43644a157b1bfaa2d054bd771f69b9
voidnullvoidnull <dimaek@li.ru>
2016-07-31 16:57:34 +0400

    init

---
+++ Assets/Scripts/Render/VoidTopology.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public struct VoidTopology {
+
+    public Vector3[] points;
+    public int[] quads;
+    public Matrix4x4 matrix;
+
+    public static VoidTopology operator + (VoidTopology first, VoidTopology second) {
+
+        int indexShift = first.points.Length;
+        List<Vector3> resultPoints = new List<Vector3>(first.points);
+        resultPoints.AddRange(second.points);
+
+        List<int> secondShiftedQuads = new List<int>();
+        foreach(int p in second.quads) {
+            secondShiftedQuads.Add(p+indexShift);
+        }
+        List<int> resultQuads = new List<int>(first.quads);
+        resultQuads.AddRange(secondShiftedQuads);
+
+        return new VoidTopology { points = resultPoints.ToArray(), quads = resultQuads.ToArray()};
+    }
+}
